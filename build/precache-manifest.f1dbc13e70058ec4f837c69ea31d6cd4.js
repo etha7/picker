4504ce10d3c0691cdbec128b48a4e2c7bb506beb
Ethan DeSon <ethandeson@gmail.com>
2020-03-19 01:31:00 -0700

    attempting to deploy with new package.json that will start express server then start serving react frontend

---
+++ build/precache-manifest.f1dbc13e70058ec4f837c69ea31d6cd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e98cdf581e6140ea5269869be83bb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c7644ba744bd7a91388",
+    "url": "/static/css/main.a633ab3c.chunk.css"
+  },
+  {
+    "revision": "3d60c48504a2ddc86a6a",
+    "url": "/static/js/2.065f96ae.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.065f96ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c7644ba744bd7a91388",
+    "url": "/static/js/main.7f16232d.chunk.js"
+  },
+  {
+    "revision": "0caebaadc1b41caf74ec",
+    "url": "/static/js/runtime-main.162d7bed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
